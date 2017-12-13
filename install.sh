e2aedccf76ac0e97004ade9886211b08ada2010a
Vladyslav Aleksakhin <vadyalex@gmail.com>
2017-12-13 16:16:37 +0100

    add installation script

---
+++ install.sh
@@ -0,0 +1,46 @@
+#
+# This installation script is based on excelent work of Oh My Zsh project.
+#
+
+main() {
+  
+  # Use colors, but only if connected to a terminal, and that terminal supports them.
+  if which tput >/dev/null 2>&1; then
+      ncolors=$(tput colors)
+  fi
+  if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
+  else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    BOLD=""
+    NORMAL=""
+  fi
+
+  hash sudo > /dev/null 2>&1 || {
+	printf "${RED}Install sudo command to be able to run script${NORMAL}\n"
+	exit 1
+  }
+
+  printf "${YELLOW}Installing git and make..${NORMAL}\n"
+  sudo apt update && sudo apt install -y \
+	  git \
+	  make
+
+  printf "${YELLOW}Cloning dotfiles...${NORMAL}\n"
+  env git clone --depth=1 https://github.com/vadyalex/dotfiles.git ~/.dotfiles
+
+  cd ~/.dotfiles
+
+  make
+
+}
+
+main
